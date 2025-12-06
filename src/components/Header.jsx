f6d418c88cbfb21b72dd88356a9747265ab5dc06
Binayak Bhattacharjee <binayakb1604@gmail.com>
2025-12-06 13:23:22 +0530

    Update header text to include 'freaking'

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,7 +9,7 @@ const Header = () => {
         <img src={DoubletickLogo} alt="Doubletick Logo" className="logo" />
       </div>
       <div style={{ display: 'flex', alignItems: 'center', padding: '10px 0' }}>
-        <span style={{ fontSize: '1.2rem', fontWeight: 'bold', color: 'black' }}>All Customers</span>
+        <span style={{ fontSize: '1.2rem', fontWeight: 'bold', color: 'black' }}>All freaking Customers</span>
         <span style={{ color: 'green', marginLeft: '10px', fontSize: '1.5rem', fontWeight: 'bold' }}>1230</span>
       </div>
     </>
